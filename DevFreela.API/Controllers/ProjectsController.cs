14a4a2ae0ae7fd2b85020aecb96ba562b85f6e7c
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-26 14:04:27 -0300

    criação do controller

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+	[Route("/api/projects")]
+	public class ProjectsController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok();
+		}
+	}
+}
