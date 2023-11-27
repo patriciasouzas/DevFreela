14a0d6502bb27af7cbbbf868a32014d16013bb82
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-26 23:58:45 -0300

    alterações no controller - adicionando mais ações

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
@@ -6,9 +7,48 @@ namespace DevFreela.API.Controllers
 	public class ProjectsController : ControllerBase
 	{
 		[HttpGet]
-		public IActionResult Get()
+		public IActionResult Get(string query)
 		{
 			return Ok();
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] CreateProjectModel createProject)
+		{
+			if (createProject.Title.Length > 50)
+			{
+				return BadRequest();
+			}
+
+			return CreatedAtAction
+				(
+				nameof(GetById),
+				new { id = createProject.Id },
+				createProject
+				);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+		{
+			if (updateProject.Description.Length > 200)
+			{
+				return BadRequest();
+			}
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			return NoContent();
 		}
 	}
 }
