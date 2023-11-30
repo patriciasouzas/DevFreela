4267b7003afb81faddbe100d511989bdcd84166a
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-29 23:40:40 -0300

    ajustes

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,12 +1,24 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
 	[Route("/api/projects")]
 	public class ProjectsController : ControllerBase
 	{
-		[HttpGet]
+		private readonly OpeningTimeOption _option;
+        public ProjectsController(IOptions<OpeningTimeOption> option)
+        {
+			_option = option.Value;
+        }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="query"></param>
+		/// <returns></returns>
+        [HttpGet]
 		public IActionResult Get(string query)
 		{
 			return Ok();
