b7090a40497f7b6a03b69922703acd3742d55e6a
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-29 21:10:51 -0300

    ajustes e criação de novos models

---
+++ DevFreela.API/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace DevFreela.API.Models
+{
+	public class LoginModel
+	{
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
