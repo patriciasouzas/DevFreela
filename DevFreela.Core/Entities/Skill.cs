bfa48c423a0c59750b3949e0c5acfdd41a0e0ccf
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-03 14:31:54 -0300

    criação de Core

---
+++ DevFreela.Core/Entities/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevFreela.Core.Entities
+{
+	public class Skill
+	{
+		public Skill(string description)
+		{
+			Description = description;
+			CreatedAt = DateTime.Now;
+		}
+		public string Description { get; private set; }
+		public DateTime CreatedAt { get; private set; }
+	}
+}
