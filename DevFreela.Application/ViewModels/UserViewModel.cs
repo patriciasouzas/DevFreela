815eda5c64837ae5e7065f5befcb95f47b96d113
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-25 20:58:39 -0300

    criação de input e view para user

---
+++ DevFreela.Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DevFreela.Application.ViewModels
+{
+	public class UserViewModel
+	{
+		public UserViewModel(string fullname, string email)
+		{
+			FullName = fullname;
+			Email = email;
+		}
+
+		public string FullName { get; private set; }
+		public string Email { get; private set; }
+	}
+}
