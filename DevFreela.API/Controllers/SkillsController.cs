4095b50b82efe995b76addb76d03ce802a9369c9
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-25 22:13:31 -0300

    criação do endpoint de skills

---
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+	[Route("api/skills")]
+	public class SkillsController : ControllerBase
+	{
+		private readonly ISkillService _skillService;
+		public SkillsController(ISkillService skillService)
+		{
+			_skillService = skillService;
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var Skills = _skillService.GetAll();
+
+			return Ok(Skills);
+		}
+	}
+}
