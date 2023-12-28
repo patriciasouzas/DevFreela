cec2bd266c6a76691208c19ad50367ab13f12f46
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-27 21:32:59 -0300

    criação de classes para utilizar mediatr

---
+++ DevFreela.Application/Commands/CreateProject/CreateProjectHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace DevFreela.Application.Commands.CreateProject
+{
+	public class CreateProjectHandler : IRequestHandler<CreateProjectCommand, int>
+	{
+		public Task<int> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
