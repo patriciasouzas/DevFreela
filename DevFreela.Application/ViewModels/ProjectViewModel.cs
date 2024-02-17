697de21210ab95f8d5f78e86ceffd1bf75fcf5c6
patriciasouzas <patriciasouzaa89@gmail.com>
2024-02-17 18:16:24 -0300

    ajustes no project view model

--- DevFreela.Application/ViewModels/ProjectViewModel.cs
+++ DevFreela.Application/ViewModels/ProjectViewModel.cs
@@ -1,18 +1,16 @@
-﻿using System;
-
-namespace DevFreela.Application.ViewModels
+﻿namespace DevFreela.Application.ViewModels
 {
 	public class ProjectViewModel
 	{
 		public ProjectViewModel(int id, string title, DateTime createdAt)
 		{
-			Id = Id;
+			Id = id;
 			Title = title;
 			CreatedAt = createdAt;
 		}
 
-        public int Id { get; private set; }
-        public string Title { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-    }
+		public int Id { get; private set; }
+		public string Title { get; private set; }
+		public DateTime CreatedAt { get; private set; }
+	}
 }
