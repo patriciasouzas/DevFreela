d99b8d63f386a4d697ac2d7937b69e183157b65b
patriciasouzas <patriciasouzaa89@gmail.com>
2024-02-17 15:25:39 -0300

    criação de diretório para gerenciar as queries

---
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQuery.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Application.ViewModels;
+using MediatR;
+
+namespace DevFreela.Application.Queries.GetAllProjects
+{
+	public class GetAllProjectsQuery : IRequest<List<ProjectViewModel>>
+	{
+		public GetAllProjectsQuery(string query)
+		{
+			Query = query;
+		}
+		public string Query { get; set; }
+	}
+}
---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using DevFreela.Application.ViewModels;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace DevFreela.Application.Queries.GetAllSkills
+{
+	public class GetAllSkillsQueryHandler : IRequestHandler<GetAllSkillsQuery, List<SkillViewModel>>
+	{
+		//private readonly DevFreelaDbContext _dbContext;
+		private readonly string _connectionString;
+		public GetAllSkillsQueryHandler(IConfiguration configuration)
+		{
+			_connectionString = configuration.GetConnectionString("DevFreelaCs");
+		}
+		public async Task<List<SkillViewModel>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+		{
+			using var mySqlConnection = new MySqlConnection(_connectionString);
+			{
+				mySqlConnection.Open();
+
+				var script = "SELECT Id, Description FROM Skills";
+
+				var skills = await mySqlConnection.QueryAsync<SkillViewModel>(script);
+
+				return skills.ToList();
+			}
+		}
+	}
+}
