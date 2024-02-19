4f179ed3e8322eeac2901b5ac8fae24e798c3628
patriciasouzas <patriciasouzaa89@gmail.com>
2024-02-18 23:27:31 -0300

    modificação: refatoração de código

--- DevFreela.Application/Commands/StartProject/StartProjectCommandHandler.cs
+++ DevFreela.Application/Commands/StartProject/StartProjectCommandHandler.cs
@@ -1,36 +1,23 @@
-﻿using Dapper;
-using DevFreela.Infrastructure.Persistence;
+﻿using DevFreela.Core.Repositories;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using MySqlConnector;
 
 namespace DevFreela.Application.Commands.StartProject
 {
 	public class StartProjectCommandHandler : IRequestHandler<StartProjectCommand, Unit>
 	{
-		private readonly DevFreelaDbContext _dbContext;
-		private readonly string _connectionString;
-		public StartProjectCommandHandler(DevFreelaDbContext dbContext, IConfiguration configuration)
+		private readonly IProjectRepository _projectRepository;
+		public StartProjectCommandHandler(IProjectRepository projectRepository)
 		{
-			_dbContext = dbContext;
-			_connectionString = configuration.GetConnectionString("DevFreelaCs");
+			_projectRepository = projectRepository;
 		}
 
 		public async Task<Unit> Handle(StartProjectCommand request, CancellationToken cancellationToken)
 		{
-			var project = await _dbContext.Projects.SingleOrDefaultAsync(p => p.Id == request.Id);
+			var project = await _projectRepository.GetByIdAsync(request.Id);
 
 			project.Start();
 
-			using (var mySqlConnection = new MySqlConnection(_connectionString))
-			{
-				mySqlConnection.Open();
-
-				var script = "UPDATE Projects SET Status = @status, StartedAt = @startedat WHERE Id = @id";
-
-				mySqlConnection.Execute(script, new { status = project.Status, startedAt = project.StartedAt, request.Id });
-			}
+			await _projectRepository.StartAsync(project);
 
 			return Unit.Value;
 		}
