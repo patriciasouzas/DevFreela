23e12a7910910bd5fd2f0d4e29d69234d223aced
patriciasouzas <patriciasouzaa89@gmail.com>
2024-02-26 18:25:44 -0300

    criação de camada de validação

---
+++ DevFreela.Application/Validators/CreateUserCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Application.Commands.CreateUser;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace DevFreela.Application.Validators
+{
+	public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+	{
+		public CreateUserCommandValidator()
+		{
+			RuleFor(p => p.Email)
+				.NotEmpty()
+				.EmailAddress()
+				.WithMessage("E-mail não válido!");
+
+			RuleFor(p => p.Password)
+				.Must(ValidPassword)
+				.WithMessage("Senha deve conter pelo menos 8 caracteres, um número, uma letra maiúscula, uma minúscula e um caracter especial.");
+
+			RuleFor(p => p.FullName)
+				.NotNull()
+				.NotEmpty()
+				.WithMessage("Nome é obrigatório.");
+		}
+
+		public bool ValidPassword(string password)
+		{
+			var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+			return regex.IsMatch(password);
+		}
+	}
+}
