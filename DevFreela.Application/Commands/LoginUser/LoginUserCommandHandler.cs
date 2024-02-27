f8a5a2e52abc4e1a03f967e23d2fe5e10d181851
patriciasouzas <patriciasouzaa89@gmail.com>
2024-02-27 18:05:16 -0300

    adicionado ajustes de login do user para autenticação

---
+++ DevFreela.Application/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using MediatR;
+
+namespace DevFreela.Application.Commands.LoginUser
+{
+	public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, LoginUserViewModel>
+	{
+		private readonly IAuthService _authService;
+		private readonly IUserRepository _userRepository;
+		public LoginUserCommandHandler(IAuthService authService, IUserRepository userRepository)
+		{
+			_authService = authService;
+			_userRepository = userRepository;
+		}
+		public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+		{
+			// utilizar mesmo algoritmo para criar hash dessa senha
+			var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+			// buscar no meu banco de dados um user que tenha e-mail e senha em formato hash
+			var user = await _userRepository.GetUserByEmailAndPasswordAsync(request.Email, passwordHash);
+
+			// se não existir, erro no login
+			if (user == null)
+			{
+				return null;
+			}
+
+			// se existir, gera o token usando as informações do usuário
+			var token = _authService.GenerateJwtToken(user.Email, user.Role);
+
+			return new LoginUserViewModel(user.Email, token);
+		}
+	}
+}
