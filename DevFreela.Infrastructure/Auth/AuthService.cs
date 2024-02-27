2e13f38879afe71bd8425458de7fdfbf0059f3d4
patriciasouzas <patriciasouzaa89@gmail.com>
2024-02-26 22:54:27 -0300

    alterações relacionadas a implementação do jwt

---
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace DevFreela.Infrastructure.Auth
+{
+	public class AuthService : IAuthService
+	{
+		private readonly IConfiguration _configuration;
+		public AuthService(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public string GenerateJwtToken(string email, string role)
+		{
+			var issuer = _configuration["Jwt:Issuer"];
+			var audience = _configuration["Jwt:Audience"];
+			var key = _configuration["Jwt:Key"];
+
+			var secutirtyKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+		}
+	}
+}
