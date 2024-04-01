3234b2e0abaccbfd088df3d1d16edc1d2e373221
patriciasouzas <patriciasouzaa89@gmail.com>
2024-03-31 21:35:24 -0300

    criação de dto para pagamento

---
+++ DevFreela.Core/DTOs/PaymentInfoDto.cs
@@ -0,0 +1,20 @@
+﻿namespace DevFreela.Core.DTOs
+{
+	public class PaymentInfoDto
+	{
+		public PaymentInfoDto(int id, string creditCardNumber, string cvv, string expiresAt, string fullName)
+		{
+			Id = id;
+			CreditCardNumber = creditCardNumber;
+			Cvv = cvv;
+			ExpiresAt = expiresAt;
+			FullName = fullName;
+		}
+
+		public int Id { get; set; }
+		public string CreditCardNumber { get; set; }
+		public string Cvv { get; set; }
+		public string ExpiresAt { get; set; }
+		public string FullName { get; set; }
+	}
+}
