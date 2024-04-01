e13322b8004f4bdc5cf6895ff2cf84d2090bafe9
patriciasouzas <patriciasouzaa89@gmail.com>
2024-03-31 21:36:20 -0300

    criação da camada de pagamento

---
+++ DevFreela.Infrastructure/Payments/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Core.DTOs;
+using DevFreela.Core.Services;
+
+namespace DevFreela.Infrastructure.Payments
+{
+	public class PaymentService : IPaymentService
+	{
+		public Task<bool> ProcessPayment(PaymentInfoDto paymentInfDTO)
+		{
+			// TODO lógica de pagamento com Gateway de pagamento
+			return Task.FromResult(true);
+		}
+	}
+}
