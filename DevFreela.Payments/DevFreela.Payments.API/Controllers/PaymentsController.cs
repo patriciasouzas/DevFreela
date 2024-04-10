178b6f15ae7720cae44a2afee9e8eb86ed733a89
patriciasouzas <patriciasouzaa89@gmail.com>
2024-04-09 22:47:10 -0300

    ajuste na criação do controlador de pagamentos

--- DevFreela.Payments/DevFreela.Payments.API/Controllers/PaymentsController.cs
+++ DevFreela.Payments/DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.Payments.API.Model;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.Payments.API.Controllers
 {
+	[Route("api/payments")]
 	public class PaymentsController : ControllerBase
 	{
+		private readonly IPaymentService _paymentService;
+		public PaymentsController(IPaymentService paymentService)
+		{
+			_paymentService = paymentService;
+		}
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+		{
+			var result = await _paymentService.Process(paymentInfoInputModel);
+
+			if (!result)
+			{
+				return BadRequest();
+			}
+			return NoContent();
+		}
 	}
 }
---
+++ DevFreela.Payments/DevFreela.Payments.API/Services/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Payments.API.Model;
+
+namespace DevFreela.Payments.API.Services
+{
+	public interface IPaymentService
+	{
+		Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel);
+	}
+}
