573784f276c1ca014971d4b22dbd8766ba9bbb8a
patriciasouzas <patriciasouzaa89@gmail.com>
2024-04-09 22:43:33 -0300

    refatoração para construir processamento de pagamento

--- DevFreela.Infrastructure/Payments/PaymentService.cs
+++ DevFreela.Infrastructure/Payments/PaymentService.cs
@@ -1,14 +1,36 @@
 ﻿using DevFreela.Core.DTOs;
 using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
 
 namespace DevFreela.Infrastructure.Payments
 {
 	public class PaymentService : IPaymentService
 	{
-		public Task<bool> ProcessPayment(PaymentInfoDto paymentInfDTO)
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly string _paymentsBaseUrl;
+		public PaymentService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
 		{
-			// TODO lógica de pagamento com Gateway de pagamento
-			return Task.FromResult(true);
+			_httpClientFactory = httpClientFactory;
+			_paymentsBaseUrl = configuration.GetSection("Services:Payments").Value;
+		}
+		public async Task<bool> ProcessPayment(PaymentInfoDto paymentInfDTO)
+		{
+			var url = $"{_paymentsBaseUrl}/api/payments";
+			var paymentInfoJson = JsonSerializer.Serialize(paymentInfDTO);
+
+			var paymentInfoContent = new StringContent(
+				paymentInfoJson,
+				Encoding.UTF8,
+				"application/json"
+				);
+
+			var httpClient = _httpClientFactory.CreateClient("Payments");
+
+			var response = await httpClient.PostAsync(url, paymentInfoContent);
+
+			return response.IsSuccessStatusCode;
 		}
 	}
 }
