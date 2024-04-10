933c0ac1ca712064524da49c4f53b97091baa425
patriciasouzas <patriciasouzaa89@gmail.com>
2024-04-09 22:42:00 -0300

    ajuste na classe de payments

--- DevFreela.Core/DTOs/PaymentInfoDto.cs
+++ DevFreela.Core/DTOs/PaymentInfoDto.cs
@@ -2,19 +2,21 @@
 {
 	public class PaymentInfoDto
 	{
-		public PaymentInfoDto(int id, string creditCardNumber, string cvv, string expiresAt, string fullName)
+		public PaymentInfoDto(int idProject, string creditCardNumber, string cvv, string expiresAt, string fullName, decimal amount)
 		{
-			Id = id;
+			IdProject = idProject;
 			CreditCardNumber = creditCardNumber;
 			Cvv = cvv;
 			ExpiresAt = expiresAt;
 			FullName = fullName;
+			Amount = amount;
 		}
 
-		public int Id { get; set; }
+		public int IdProject { get; set; }
 		public string CreditCardNumber { get; set; }
 		public string Cvv { get; set; }
 		public string ExpiresAt { get; set; }
 		public string FullName { get; set; }
+		public decimal Amount { get; set; }
 	}
 }
